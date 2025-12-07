69c347fc8185847b7d66eaf6a44ea76eccfa382a
Bernd Beuster <bernd.beuster@gmail.com>
2025-12-07 14:21:10 +0100

    Remove reference for int parameters

--- src/coord.hpp
+++ src/coord.hpp
@@ -14,11 +14,11 @@ struct coord_t {
     return {.x = lhs.x + rhs.x, .y = lhs.y + rhs.y};
   }
 
-  friend coord_t operator*(const int &lhs, const coord_t &rhs) {
+  friend coord_t operator*(const int lhs, const coord_t &rhs) {
     return {.x = lhs * rhs.x, .y = lhs * rhs.y};
   }
 
-  friend coord_t operator*(const coord_t &lhs, const int &rhs) {
+  friend coord_t operator*(const coord_t &lhs, const int rhs) {
     return {.x = lhs.x * rhs, .y = lhs.y * rhs};
   }
 };
